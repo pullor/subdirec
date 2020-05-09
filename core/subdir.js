d41b1d9ba62a41a83a942628df5423340e130d92
xingzhe <xingzhe@webuy.ai>
2020-05-09 12:57:20 +0800

    feat: update

--- core/subdir.js
+++ core/subdir.js
@@ -5,8 +5,9 @@ const inquirer = require("inquirer");
 const tools = require('./tools');
 
 const execute = (list, command) => {
+  tools.dchalkGreen(`\n命令执行中请耐心等待...\n`);
   list.forEach(path => {
-    exec(`cd ${path}; ${command};`, function(error, stdout, stderr){
+    exec(`${command};`, { cwd: path }, function(error, stdout, stderr){
       if(error || stdout || stderr) tools.dchalkGreen(`\n目录：${path}`);
       if(error) tools.dchalkRed(error);
       if(stderr) tools.dchalkRed(stderr);
@@ -27,18 +28,18 @@ const askQuestions = () => {
 };
 
 const getDirList = (path = process.cwd()) => {
-  return fs.readdirSync(path).filter(item => {
-    return fs.existsSync(path+'/'+ item + '/package.json');
-  }).map(item => path+'/'+ item );
+  return fs.readdirSync(path)
+          .filter(item =>  fs.existsSync(path+'/'+ item + '/package.json'))
+          .map(item => path+'/'+ item );
 }
 
 const run = async () => {
   const command = process.argv[2];
-  tools.dchalkGreen(`\n当前目录：${process.cwd()}`);
+  tools.dchalkGreen(`\n当前目录：\n${process.cwd()}`);
 
   const dirList = getDirList();
   tools.dchalkGreen(`\n执行目录：\n${dirList.join('\n')}`);;
-  tools.dchalkGreen(`\n执行命令：${command}\n`);
+  tools.dchalkGreen(`\n执行命令：\n${command}\n`);
 
   const answers = await askQuestions();
   const { FILENAME } = answers;
